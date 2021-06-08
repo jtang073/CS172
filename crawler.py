1d5ef0e0e236cb1ac3c7d108dfb0eb64c2e7ecf7
Jason Tang <jtang073@ucr.edu>
2021-06-07 17:48:47 -0700

    Added robots.txt feature

--- crawler.py
+++ crawler.py
@@ -6,7 +6,8 @@ from bs4 import BeautifulSoup
 import csv
 import json
 import time
-
+import urllib.parse
+import urllib.robotparser
 
 seedFile = ""
 seedUrl = ""
@@ -27,7 +28,7 @@ with open(seedFile) as f:
     seedUrls = f.readlines()
     f.close()
 
-fff = open(outputFile, "w") #open brackets for json data
+fff = open(outputFile, "w")     #open brackets for json data
 fff.write("[\n")
 fff.close()
 
@@ -40,34 +41,61 @@ for seeds in seedUrls:
     urlQueue.append(seeds.rstrip())
     duplicateList.append(seeds.rstrip())
     currHop = 0
-    while (currPage < maxPages) and (currHop < maxHops) and len(urlQueue) > 0:          #3 limits: exceeding max pages, exceeding max hops, empty url queue
+    while (currPage < maxPages) and (currHop < maxHops) and len(urlQueue) > 0:      #3 limits: exceeding max total pages, exceeding max seed hops, or empty url queue from seed
         print("Crawling: " + str(urlQueue[0]))
+        duplicateList.append(urlQueue[0])
         try:
-            page = requests.get(urlQueue[0])
-            soup = BeautifulSoup(page.content, 'html.parser')
-            text = soup.get_text().replace("\n", " ").replace("\t", " ").replace("\r", " ")
-            text = " ".join(re.findall(r'\w+(?:\.?\w+)*', text))
-            title = str(soup.title).replace("<title>", "").replace("</title>", "")
-            tempJson = {"title": title, "url": urlQueue[0], "text": text}
-            realJson = json.dumps(tempJson)
-            with open(outputFile, 'a') as ff:
-                ff.writelines(realJson + "\n")
-                ff.close()
-            currPage += 1
-            currHop += 1
-            urlQueue.pop(0)
-            for link in soup.find_all('a'):
+            ########### Reading Robots.txt ###########
+            numSlashes = 0
+            robotUrl = ""
+            for chars in urlQueue[0]:
+                if numSlashes < 3:
+                    if chars in '/':
+                        numSlashes += 1
+                    robotUrl += chars
+            if robotUrl[-1] not in '/':
+                robotUrl += '/'
+            robotUrl = robotUrl + "robots.txt"
+            rp = urllib.robotparser.RobotFileParser()
+            rp.set_url(robotUrl)
+            rp.read()
+            """
+            ### DEBUG ###
+            print("Reading: " + str(robotUrl))
+            #############
+            """
+            ##########################################
+            if rp.can_fetch("*", urlQueue[0]):
                 try:
-                    if "http://" in link.get('href') or "https://" in link.get('href'):     #some links don't contain full url but http/s does
-                        if link.get('href') not in duplicateList:
-                            urlQueue.append(link.get('href'))
-                            duplicateList.append(link.get('href'))
+                    domain = urllib.parse.urlparse(urlQueue[0]).netloc
+                    page = requests.get(urlQueue[0])
+                    soup = BeautifulSoup(page.content, 'html.parser')
+                    text = soup.get_text().replace("\n", " ").replace("\t", " ").replace("\r", " ")
+                    text = " ".join(re.findall(r'\w+(?:\.?\w+)*', text))
+                    title = str(soup.title).replace("<title>", "").replace("</title>", "")
+                    tempJson = {"title": title, "url": urlQueue[0], "text": text}
+                    realJson = json.dumps(tempJson)
+                    with open(outputFile, 'a') as ff:
+                        ff.writelines(realJson + "\n")
+                        ff.close()
+                    currPage += 1
+                    currHop += 1
+                    for link in soup.find_all('a'):
+                        try:
+                            if "http://" in link.get('href') or "https://" in link.get('href'):     #some links don't contain full url but http/s does
+                                if link.get('href') not in duplicateList:
+                                    urlQueue.append(link.get('href'))
+                                    #duplicateList.append(link.get('href'))
+                        except:
+                            pass        #skip over all <a> headers with no href links
                 except:
-                    pass
+                    print("Error with crawling: " + str(urlQueue[0]))       #Errors with requests.get (ie: uncrawlable page or 404 etc)
+                time.sleep(1)       #Delay 1 second so we don't access sites too fast
+            else:
+                print("Warning: Robots.txt disallowed crawling: " + str(urlQueue[0]))
         except:
-            print("Error with crawling: " + str(urlQueue[0]))
-            urlQueue.pop(0)
-        time.sleep(1)
+            print("Error: Skipping URL because there was an error with with loading " + urlQueue[0])      #skip to be safe
+        urlQueue.pop(0)     #remove first in queue whether crawl was successful or not
     """
     ### DEBUG ###
     for urls in urlQueue:
@@ -75,9 +103,9 @@ for seeds in seedUrls:
     print(len(urlQueue))
     #############
     """
-    urlQueue = []   #empty urlQueue to restart
+    urlQueue = []       #empty urlQueue to restart
 
-with open(outputFile, 'a') as ffff: #close brackets for json data
+with open(outputFile, 'a') as ffff:     #close brackets for json data
     ffff.write("]")
     ffff.close()
 
